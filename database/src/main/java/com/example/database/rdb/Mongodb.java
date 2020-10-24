d259288b9f80185489a598bcd1fb5c3d2928ca0d
yanghuaiGit <2774584057@qq.com>
2020-10-24 16:49:19 +0800

    增加kerberos相关代码

--- database/src/main/java/com/example/database/rdb/Mongodb.java
+++ database/src/main/java/com/example/database/rdb/Mongodb.java
@@ -1,5 +1,7 @@
 package com.example.database.rdb;
 
+import com.example.database.util.GsonUtil;
+import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
@@ -8,50 +10,84 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class Mongodb {
 
 
     public static void main(String[] args) {
+
+
+
+
         MongoClient mongoClient = getClientWithUrl("mongodb://root:abc123@kudu5:27017/tudou?authSource=admin");
         //连接到数据库
         MongoDatabase mongoDatabase = mongoClient.getDatabase("tudou");
 
-        MongoCollection<Document> collection = mongoDatabase.getCollection("test");
+        MongoCollection<Document> collection = mongoDatabase.getCollection("30707");
 
 
-        ArrayList arrayList = new ArrayList<>(12);
+        ArrayList arrayList = new ArrayList<>(2);
         for (int i = 0; i < 10; i++) {
-            arrayList.add(new Document("id", "123")
-                    .append("user_id", 18)
+            arrayList.add(new Document("id", i)
+                    .append("user_id", 18 + i)
                     .append("name", new Document().append("key1", "h1")));
         }
+
         //创建文档
-        Document document = new Document("id", "123")
-                .append("user_id", 18)
-                .append("list", arrayList)
-                .append("name", new Document().append("key1", "h1"));
+        Document CountryValue = new Document().append("Code", "beijin");
 
+        Document Country = new Document().append("CountryType", "Country of Asia").append("CountryValue", CountryValue);
+
+        Document CountryDetails = new Document()
+                .append("Country", Country);
+
+        Document Description = new Document().append("Description",new Document().append("decription1","3").append("decription2",4));
+
+
+//select * from `30707`
         //insert
-        collection.insertOne(document);
-
+        for (int i = 0; i < 100; i++) {
+            HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+            objectObjectHashMap.put("json","json1");
+            Document document = new Document("ActiveStatus", "Inactive")
+                    .append("CountryDetails", CountryDetails)
+                    .append("Descriptions",Description)
+                    .append("name", arrayList)
+                    .append("map",objectObjectHashMap)
+                    .append("time",new Date())
+                    .append("haha",new Document().append("time",new Date()));
+            GsonUtil.GSON.fromJson((String) document.toJson(), GsonUtil.gsonMapTypeToken);
+//            collection.insertOne(document);
+        }
+        Gson gson = new Gson();
         FindIterable findIterable = collection.find();
         MongoCursor cursor = findIterable.iterator();
         while (cursor.hasNext()) {
-            System.out.println(cursor.next());
+            Document next = (Document) cursor.next();
+            String[] names = next.keySet().toArray(new String[0]);
+            for (int i = 0; i < names.length; i++) {
+                Object tempdata = next.get(names[i]);
+                if (tempdata instanceof List) {
+                    System.out.println(gson.toJson(tempdata));
+                } else if (tempdata instanceof Map) {
+                    System.out.println(gson.toJson(tempdata));
+                } else if (tempdata instanceof Document) {
+                    System.out.println(((Document) tempdata).toJson());
+                } else {
+                    System.out.println(tempdata);
+                }
+            }
+            System.out.println("---------------");
         }
 
         //申明删除条件
-        Bson filter = Filters.eq("name", "张三");
-        //删除与筛选器匹配的所有文档
-        collection.deleteMany(filter);
+//        Bson filter = Filters.eq("id", "123");
+//        //删除与筛选器匹配的所有文档
+//        collection.deleteMany(filter);
 
 
     }
---
+++ hdfs/src/main/java/com/example/hdfs/orc/OrcWriter.java
@@ -0,0 +1,41 @@
+package com.example.hdfs.orc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
+import org.apache.orc.CompressionKind;
+import org.apache.orc.OrcFile;
+import org.apache.orc.TypeDescription;
+import org.apache.orc.Writer;
+
+public class OrcWriter {
+    public static void main(String[] args) throws Exception {
+        Path testFilePath = new Path("/tmp/test.orc");
+        Configuration conf = new Configuration();
+        TypeDescription schema = TypeDescription.fromString("struct<field1:int,field2:int,field3:int>");
+        Writer writer = OrcFile.createWriter(testFilePath, OrcFile.writerOptions(conf).setSchema(schema).compress(CompressionKind.SNAPPY));
+        VectorizedRowBatch batch = schema.createRowBatch();
+        LongColumnVector first = (LongColumnVector) batch.cols[0];
+        LongColumnVector second = (LongColumnVector) batch.cols[1];
+        LongColumnVector third = (LongColumnVector) batch.cols[2];
+
+        final int BATCH_SIZE = batch.getMaxSize();
+        // add 1500 rows to file
+        for (int r = 0; r < 15000000; ++r) {
+            int row = batch.size++;
+            first.vector[row] = r;
+            second.vector[row] = r * 3;
+            third.vector[row] = r * 6;
+            if (row == BATCH_SIZE - 1) {
+                writer.addRowBatch(batch);
+                batch.reset();
+            }
+        }
+        if (batch.size != 0) {
+            writer.addRowBatch(batch);
+            batch.reset();
+        }
+        writer.close();
+    }
+}
